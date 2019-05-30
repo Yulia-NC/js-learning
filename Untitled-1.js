2bcdfca3f4e5f7e4e0ec1355bd4abc537e9f33b6
Yulia K <workmail.yulia@gmail.com>
2019-05-30 22:43:37 +0300

    Create Untitled-1.js

---
+++ Untitled-1.js
@@ -0,0 +1,86 @@
+3 ** 2; // 9
+2 **4; // 16
+Math.pow(2, 4); //16
+Math.pow(3, 2);// 9
+
+const e = 2.7;
+const pi = 3.14;
+const kk = pi * e;
+console.log(kk);
+
+const a;
+console.log(a);
+
+console.log (11 % 3 **2)
+
+1 + true
+false + false
+3 / 0
+Infinity * Infinity // Infinity
+Infinity * 0 // NaN
+-2 ** 2; invalid
+-(2 ** 2);
+
+const weight = 48;
+const c = 300000000;
+const energy = weight * Math.pow(c, 2);
+console.log(energy);
+
+const surfacearea = (radius) =>{
+    return 4 * 3.14 * radius * radius; //определение функции
+}
+const surfaceMars = surfacearea(3390); //вызов функции
+
+const percentage = (number, total) => {
+    return number * 100 / total;
+}
+console.log("How much of may is gone?");
+console.log(persentage(30, 31));
+
+//const identity = (value) => {
+//    return value;
+//};
+
+//const identity = function(value) {
+//    return value;
+//};
+
+//function identity(value) {
+//    return value;
+//}
+
+//const identity = value => value; (если у функции 1 аргумент)
+
+const squareOfSum = (a, b) => {
+    return Math.pow(a, 2) + (2 * a * b) + Math.pow(b, 2);
+  }
+  console.log(squareOfSum(2, 3));
+  console.log(squareOfSum(1, 10));
+
+const abs = num => { 
+    if (num > 0) {
+        return num;
+    } else if (num < 0) {
+        return -num;
+    } else {
+        return 0;
+    }
+}
+const abs = (num) => {          
+    if (num === 0 || num > 0) {
+        return num;
+    } else {
+        return -num;
+    }
+}
+const absValue = (num >=0) ? num : -num
+let absValue;
+
+if (num >=0) {
+    absValue = num;
+} else {
+    absValue = -num;
+}
+const getAbs = num => (num >=0) ? num : -num;
+
+const type = (num > 0) ? 'positive' : (num < 0) ? 'negative' : 'zero';
